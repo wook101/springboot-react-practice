c98629bd413e462ac2f87ee8f6f75139fd00317e
wook101 <ehddnr1022@gmail.com>
2021-11-17 01:08:09 +0900

    data.sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO BOOK(id,auther,title) values(2,'김동욱','이것이 MYSQL이다.');
-INSERT INTO BOOK(id,auther,title) values(3,'이지훈','쉽게 배우는 스프링 부트');
-INSERT INTO BOOK(id,auther,title) values(4,'이나경','Spring 5.0');
+INSERT INTO BOOK(id,auther,title) values(1,'김동욱','이것이 MYSQL이다.');
+INSERT INTO BOOK(id,auther,title) values(2,'이지훈','쉽게 배우는 스프링 부트');
+INSERT INTO BOOK(id,auther,title) values(3,'이나경','Spring 5.0');
