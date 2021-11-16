4099dad9fd8126365974ec6b2776e51594dd333c
wook101 <ehddnr1022@gmail.com>
2021-11-17 01:04:45 +0900

    컨트롤러 findAll() get요청 /book API 통합 테스트

--- src/test/java/com/wook/book/contorller/BookControllerIntegreTest.java
+++ src/test/java/com/wook/book/contorller/BookControllerIntegreTest.java
@@ -2,6 +2,8 @@ package com.wook.book.contorller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wook.book.domain.Book;
+import com.wook.book.repositroy.BookRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,6 +16,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import javax.transaction.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,8 +35,12 @@ public class BookControllerIntegreTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private BookRepository bookRepository;
+
     @Test
-    public void 컨트롤러_저장_테스트() throws Exception{
+    @DisplayName("컨트롤러 저장 통합 테스트")
+    public void sava_테스트() throws Exception{
         //given(테스트 하기위한 준비단계)
         Book book = new Book(null,"Springboot와 react","김유빈");
         String jsonData = new ObjectMapper().writeValueAsString(book); //json으로 요청해야 하기 때문에 json으로 만들기
@@ -48,4 +59,30 @@ public class BookControllerIntegreTest {
                 .andDo(MockMvcResultHandlers.print());
 
     }
+
+    @Test
+    @DisplayName("컨트롤러 전체목록 조회 통합 테스트") //h2 db에 데이터 넣고 통합 테스트
+    public void findAll() throws Exception{
+        //given
+        List<Book> books = new ArrayList<>();
+        books.add(new Book(4L,"파이썬","이동욱"));
+        books.add(new Book(5L,"자바","박동욱"));
+        books.add(new Book(6L,"C언어","김동욱"));
+        bookRepository.saveAll(books);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(get("/book")
+                .accept(MediaType.APPLICATION_JSON_UTF8));//accept는 Response, APPLICATION_JSON_UTF8 한글깨짐 방지
+
+        //then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[3].title").value("파이썬"))
+                .andExpect(jsonPath("$.[4].title").value("자바"))
+                .andExpect(jsonPath("$.[5].title").value("C언어"))
+                .andDo(MockMvcResultHandlers.print());
+
+    }
+
+
 }
