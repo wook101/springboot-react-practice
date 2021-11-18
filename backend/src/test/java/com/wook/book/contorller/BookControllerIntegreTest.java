f9563339763557c7a862c2a9fabe7d2cd710e9c8
wook101 <ehddnr1022@gmail.com>
2021-11-18 18:47:16 +0900

    springboot와 react연동 및 폴더 구성

--- backend/src/test/java/com/wook/book/contorller/BookControllerIntegreTest.java
+++ backend/src/test/java/com/wook/book/contorller/BookControllerIntegreTest.java
@@ -21,7 +21,7 @@ import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
